eaff1f988cebf2a5f30687746bbeed024a3359d4
Aziz Booker <bookeraziz@gmail.com>
2020-12-15 12:56:31 -0500

    Finshed helloWorld and reverseString tests

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str2) {
+var str=str2;
+var newStr='';
+var i;
+for(i=str.length;i>=0;i--){
+ newStr+=str.charAt(i);
+}
+return newStr;
 }
 
 module.exports = reverseString
+
