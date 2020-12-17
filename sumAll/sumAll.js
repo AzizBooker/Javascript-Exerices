28e3e59c88e2176edcdeb40e329d492e7e2c28e8
Aziz Booker <bookeraziz@gmail.com>
2020-12-17 15:58:56 -0500

    Finshed sumall Exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+var sum=0;
 
+if(min<0 || max<0 || typeof min !='number'|| typeof max !='number'){
+    return "ERROR";
+}
+if(min<max){
+for(var i=min;i<=max;i++){
+    sum+=i;
+}
+}else{
+    for(var i=max;i<=min;i++){
+        sum+=i;
+    }  
+}
+return sum;
 }
 
 module.exports = sumAll
