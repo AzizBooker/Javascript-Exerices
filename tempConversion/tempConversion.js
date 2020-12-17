fcd7538884e19a3fc743a3426b15cf9790a4bd63
Aziz Booker <bookeraziz@gmail.com>
2020-12-17 17:12:17 -0500

    Finshed temp conversion exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+ celsius=(((temp-32)*5)/9);
+ celsius=Math.round(celsius*10)/10;
+ return celsius;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+fahrenheit=(temp*1.8)+32;
+fahrenheit=Math.round(fahrenheit*10)/10;
+return fahrenheit;
 }
 
 module.exports = {
