c549e513b4381af18c421752b081ec03ee59896e
Aziz Booker <bookeraziz@gmail.com>
2020-12-20 15:51:05 -0500

    Completed ceasar javascript exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,4 +1,11 @@
-const caesar = function() {
+const caesar = function(str,shift) {
+var cipher=""
+var unicode=0
+for(var i=0;i<str.length;i++){
+    unicode=str.charCodeAt(i)
+    cipher+=String.fromCharCode(unicode+shift)
+}
+return cipher;
 
 }
 
